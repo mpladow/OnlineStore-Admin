f290d696ae79fc885168895e0691ddefec235113
Michael L <mpladow@gmail.com>
2018-05-05 15:47:13 +1000

    Create new repo for admin page

---
+++ OnlineStore-Admin/OnlineStore-Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineStore_Admin.MvcApplication" Language="C#" %>
---
+++ OnlineStore-Admin/OnlineStore-Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineStore_Admin.Startup))]
+namespace OnlineStore_Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
